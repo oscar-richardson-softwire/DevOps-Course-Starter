51b88f52fdfe7a7dae3a245d478fb0a265b5bbc4
oscar-richardson-softwire <116292912+oscar-richardson-softwire@users.noreply.github.com>
2024-02-07 16:59:13 +0000

    feat: add ability to add an item (#1)

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,18 @@
-from flask import Flask
-
+from flask import Flask, render_template, request, redirect
+from todo_app.data.session_items import get_items, add_item
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
 
-@app.route('/')
-def index():
-    return 'Hello World!'
+@app.get('/')
+def index_get():
+    items = get_items()
+    return render_template('index.html', items=items)
+
+@app.post('/')
+def index_post():
+    item = request.form.get('item')
+    add_item(item)
+    return redirect("/")
