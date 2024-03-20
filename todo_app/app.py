0ee3c9c9aa5c8581f04ce81b3df990c97e1a1f04
oscar-richardson-softwire <oscar.richardson@softwire.com>
2024-03-20 16:11:18 +0000

    refactor(part1.1): create ViewModel class and use this to render index.html

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect
+from todo_app.data.classes.ViewModel import ViewModel
 from todo_app.data.trello_items import get_items, add_item, update_item_status
 from todo_app.flask_config import Config
 
@@ -9,7 +10,8 @@ app.config.from_object(Config())
 @app.get('/')
 def index_get():
     items = get_items()
-    return render_template('index.html', items=items)
+    item_view_model = ViewModel(items)
+    return render_template('index.html', view_model=item_view_model)
 
 @app.post('/')
 def index_post():
