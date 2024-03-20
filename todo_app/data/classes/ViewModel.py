c5d693ca3c6512ad9c74eb1805ff0b269c516a62
oscar-richardson-softwire <oscar.richardson@softwire.com>
2024-03-20 18:54:07 +0000

    feat(part1.2): add properties for 'Not Started' and 'Done' items to ViewModel class

--- todo_app/data/classes/ViewModel.py
+++ todo_app/data/classes/ViewModel.py
@@ -5,3 +5,15 @@ class ViewModel:
     @property
     def items(self):
         return self._items
+    
+    @property
+    def not_started_items(self):
+        item_is_not_started = lambda item : item.status == 'Not Started'
+        not_started_items = list(filter(item_is_not_started, self._items))
+        return not_started_items
+    
+    @property
+    def done_items(self):
+        item_is_done = lambda item : item.status == 'Done'
+        done_items = list(filter(item_is_done, self._items))
+        return done_items
