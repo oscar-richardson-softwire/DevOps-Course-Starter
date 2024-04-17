084b02ef4144cd13163a13b6c22a5a7e7f9a3342
oscar-richardson-softwire <oscar.richardson@softwire.com>
2024-04-17 15:15:01 +0100

    refactor: refactor ViewModel class tests to use a pytest fixture

--- todo_app/data/classes/test_view_model.py
+++ todo_app/data/classes/test_view_model.py
@@ -1,9 +1,9 @@
+import pytest
 from todo_app.data.classes.Item import Item
 from todo_app.data.classes.ViewModel import ViewModel
 
-def test_view_model_not_started_items_property_returns_items_with_status_not_started():
+def test_view_model_not_started_items_property_returns_items_with_status_not_started(view_model_and_filtered_items):
     # Arrange
-    view_model_and_filtered_items = create_view_model_with_not_started_and_done_items()
     view_model = view_model_and_filtered_items['view_model']
     not_started_items = view_model_and_filtered_items['not_started_items']
 
@@ -13,9 +13,8 @@ def test_view_model_not_started_items_property_returns_items_with_status_not_sta
     # Assert
     assert result == not_started_items
 
-def test_view_model_done_items_property_returns_items_with_status_done():
+def test_view_model_done_items_property_returns_items_with_status_done(view_model_and_filtered_items):
     # Arrange
-    view_model_and_filtered_items = create_view_model_with_not_started_and_done_items()
     view_model = view_model_and_filtered_items['view_model']
     done_items = view_model_and_filtered_items['done_items']
 
@@ -25,7 +24,8 @@ def test_view_model_done_items_property_returns_items_with_status_done():
     # Assert
     assert result == done_items
 
-def create_view_model_with_not_started_and_done_items():
+@pytest.fixture
+def view_model_and_filtered_items():
     item_1 = Item('1', 'Test item 1', 'Not Started')
     item_2 = Item('2', 'Test item 2', 'Done')
     item_3 = Item('3', 'Test item 3', 'Not Started')
@@ -43,6 +43,3 @@ def create_view_model_with_not_started_and_done_items():
         'not_started_items': not_started_items,
         'done_items': done_items
     }
-
-
-
